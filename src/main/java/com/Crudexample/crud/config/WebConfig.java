6561b005fe3ff95251448f804e5a5741a66f2dd7
GustavoHenriqueDEV <gubochniavelho@gmail.com>
2025-01-14 15:39:43 -0300

    RespComent

--- src/main/java/com/Crudexample/crud/config/WebConfig.java
+++ src/main/java/com/Crudexample/crud/config/WebConfig.java
@@ -1,21 +1,20 @@
 package com.Crudexample.crud.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Permitir acesso tanto de localhost:3000 quanto de localhost:5173
-                registry.addMapping("/posts").allowedOrigins("http://localhost:3000", "http://localhost:5173");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permitir acesso tanto de localhost:3000 quanto de localhost:5173
+        registry.addMapping("/**") // Permite qualquer caminho da API
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")  // Pode ser importante dependendo do seu frontend
+                .allowCredentials(true); // Permitir credenciais se necessário
     }
 }
+
